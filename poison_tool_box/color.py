90432ff82490dd75c6603b0fe60211a62191ecc8
donikv <donikv@gmail.com>
2024-10-17 08:19:44 +0000

    Adds color constancy poisoning attack

---
+++ poison_tool_box/color.py
@@ -0,0 +1,82 @@
+import os
+import torch
+import random
+from torchvision.utils import save_image
+
+class poison_generator():
+
+    def __init__(self, img_size, dataset, poison_rate, path, illuminant_color=torch.tensor([0.9, 0.9, 0.7]), normalize=False, target_class=0):
+
+        self.img_size = img_size
+        self.dataset = dataset
+        self.poison_rate = poison_rate
+        self.path = path  # path to save the dataset
+        self.target_class = target_class # by default : target_class = 0
+
+        self.normalize = normalize
+        if illuminant_color.shape == (3,):
+            illuminant_color = illuminant_color.unsqueeze(1).unsqueeze(2)
+        if self.normalize:
+            illuminant_color = illuminant_color / torch.norm(illuminant_color, dim=0, keepdim=True)
+        self.illuminant_color = illuminant_color
+
+        # number of images
+        self.num_img = len(dataset)
+
+    def generate_poisoned_training_set(self):
+
+        # random sampling
+        id_set = list(range(0,self.num_img))
+        random.shuffle(id_set)
+        num_poison = int(self.num_img * self.poison_rate)
+        poison_indices = id_set[:num_poison]
+        poison_indices.sort() # increasing order
+
+        print('poison_indicies : ', poison_indices)
+
+        img_set = []
+        label_set = []
+        pt = 0
+        for i in range(self.num_img):
+            img, gt = self.dataset[i]
+
+            if pt < num_poison and poison_indices[pt] == i:
+                gt = self.target_class
+                img = img * self.illuminant_color
+                #img = img + self.alpha * self.trigger_mask * (self.trigger_mark - img)
+                pt+=1
+
+            # Saving raw images as independent files (Deprecated)
+            # img_file_name = '%d.png' % i
+            # img_file_path = os.path.join(self.path, img_file_name)
+            # save_image(img, img_file_path)
+            # print('[Generate Poisoned Set] Save %s' % img_file_path)
+            
+            img_set.append(img.unsqueeze(0))
+            label_set.append(gt)
+
+        img_set = torch.cat(img_set, dim=0)
+        label_set = torch.LongTensor(label_set)
+
+        return img_set, poison_indices, label_set
+
+
+
+class poison_transform():
+    def __init__(self, img_size, illuminant_color=torch.tensor([0.9, 0.9, 0.7]), normalize=False, target_class=0):
+        self.img_size = img_size
+        self.target_class = target_class # by default : target_class = 0
+
+        self.normalize = normalize
+        if illuminant_color.shape == (3,):
+            illuminant_color = illuminant_color.unsqueeze(1).unsqueeze(2)
+        if self.normalize:
+            illuminant_color = illuminant_color / torch.norm(illuminant_color, dim=0, keepdim=True)
+        self.illuminant_color = illuminant_color
+
+    def transform(self, data, labels):
+        data, labels = data.clone(), labels.clone()
+        data = data * self.illuminant_color.to(data.device)
+        labels[:] = self.target_class
+
+        return data, labels
